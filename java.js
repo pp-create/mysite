0ff4ec27c73cc20f85d30d3b461fa495d1f4da01
Omar Mahmoud <omar.mahmoudnn@gmail.com>
2021-09-14 00:11:11 +0200

    frist commit

---
+++ java.js
@@ -0,0 +1,28 @@
+/*alert("hello ");
+function test(x){
+    var y=x+5;
+    alert(y);
+
+}
+test(10);
+function sum(x,y){
+alert(x*y);
+}
+sum(55,0)
+var x=function(){
+    alert("12")
+}
+x()*/
+//self function يعني اول ا لوقع يشتغل هي بتشتغل مع نفسها    
+/*
+(function(){
+    alert("yy")
+})();*/
+var productName=document.getElementById("productName");
+var catgery1=document.getElementById("productCatgery");
+var price=document.getElementById("productprice");
+var des=document.getElementById("productdescripion");
+function test(){
+    console.log(productName);
+}
+test();
